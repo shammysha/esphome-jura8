f2a542568a7e150dd73d937a559148c3efbd8491
shammysha <65885873+shammysha@users.noreply.github.com>
2025-03-25 03:36:37 +0300

    Next Try

--- components/jura_coffee/jura_coffee.h
+++ components/jura_coffee/jura_coffee.h
@@ -4,24 +4,21 @@
 // using namespace homeassistant;
 
 namespace esphome {
-namespace jura_coffee {
+  namespace jura_coffee {
 
-class JuraCoffee: public PollingComponent, public UARTDevice {
-    TextSensor *xsensor6 { nullptr };
-    TextSensor *xsensor7 { nullptr };
+    class JuraCoffee: public PollingComponent, public uart::UARTDevice {
 
+      public:
+        JuraCoffee();
 
-  public:
-    JuraCoffee(UARTComponent *parent, TextSensor *sensor6, TextSensor *sensor7) : UARTDevice(parent), xsensor6(sensor6), xsensor7(sensor7) {}
+        std::string tray_status
+        std::string tank_status;
 
-    std::string tray_status
-    std::string tank_status;
+        String cmd2jura(String outbytes);
 
-    String cmd2jura(String outbytes);
-
-    void setup() override;
-    void loop() override {}
-    void update() override;
+        void setup() override;
+        void update() override;
+        void loop() override {}
 
 #ifdef USE_BUTTON
     SUB_BUTTON(left_top)
@@ -41,7 +38,7 @@ class JuraCoffee: public PollingComponent, public UARTDevice {
   SUB_SWITCH(power)
 #endif
 
+    };
 
-};
-
-}}
+  }
+}
