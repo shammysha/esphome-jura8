fe1c6717a782c7973c5fe9d7bbdb7fefe70579ed
shammysha <65885873+shammysha@users.noreply.github.com>
2025-03-25 17:54:18 +0300

    Next Try

--- components/jura_coffee/button/jura_button.cpp
+++ components/jura_coffee/button/jura_button.cpp
@@ -4,27 +4,27 @@ namespace esphome {
   namespace jura_coffee {
 
     void LeftTopButton::press_action() {
-      this->parent_->cmd2jura("FA:04");
+      String result = this->parent_->cmd2jura("FA:04");
     }
 
     void LeftMiddleButton::press_action() {
-        this->parent_->cmd2jura("FA:05");
+      String result this->parent_->cmd2jura("FA:05");
     }
 
     void LeftBottomButton::press_action() {
-      this->parent_->cmd2jura("FA:06");
+      String result = this->parent_->cmd2jura("FA:06");
     }
 
     void RightTopButton::press_action() {
-      this->parent_->cmd2jura("FA:07");
+      String result = this->parent_->cmd2jura("FA:07");
     }
 
     void RightMiddleButton::press_action() {
-      this->parent_->cmd2jura("FA:08");
+      String result = this->parent_->cmd2jura("FA:08");
     }
 
     void RightBottomButton::press_action() {
-      this->parent_->cmd2jura("FA:09");
+      String result = this->parent_->cmd2jura("FA:09");
     }
 
   }
